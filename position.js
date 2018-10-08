630098908866f0ce8c91bab3b8557b183d9287c9
John Lennie <johnlennie@Johns-MacBook-Pro.local>
2018-10-07 21:23:32 -0400

    refactored

--- position.js
+++ position.js
@@ -2,7 +2,6 @@ const myArgs = process.argv.slice(2).join(' ');
 
 function characterCount(string) {
   let output = {};
-  let keyValue = [];
   for (let i = 0; i < myArgs.length; i++) {
     let key = myArgs[i];
     if (myArgs[i] !== ' ') {
